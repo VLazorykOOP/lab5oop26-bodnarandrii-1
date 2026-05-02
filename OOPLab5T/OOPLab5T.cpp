d59fd370a1982816634efed46455b8b17245b5be
bodnarandrii <bodnar.andrii@chnu.edu.ua>
2026-05-02 15:56:41 +0300

    Translate text

--- OOPLab5T/OOPLab5T.cpp
+++ OOPLab5T/OOPLab5T.cpp
@@ -6,24 +6,24 @@ using namespace std;
 
 class Student {
 protected:
-    string surname, name, pobatykovi; 
+    string surname, name, patronymic; 
     int course;
     long long id;
 public:
-    Student() : surname(""), name(""),pobatykovi(""), course(1), id(0) {
-        cout << "Student() - викликано конструктор за замовчуванням" << endl;
+    Student() : surname(""), name(""), patronymic(""), course(1), id(0) {
+        cout << "Student() - default constructor called" << endl;
     }
     
     Student(string s, string n, string p, int c, long long i) 
-        : surname(s), name(n),pobatykovi(p), course(c), id(i) {
-        cout << "Student(параметри) - створено базовий клас" << endl;
+        : surname(s), name(n), patronymic(p), course(c), id(i) {
+        cout << "Student(parameters) - base class created" << endl;
     }
     
-    virtual ~Student() { cout << "~Student() - вилучення базового класу" << endl; }
+    virtual ~Student() { cout << "~Student() - base class destruction" << endl; }
 
     virtual void print() const {
-        cout << "ПIБ: " << surname << " " << name << " " <<pobatykovi 
-             << " | Курс: " << course << " | ID: " << id << endl;
+        cout << "Name: " << surname << " " << name << " " << patronymic 
+             << " | Course: " << course << " | ID: " << id << endl;
     }
     void setID(long long new_id) { id = new_id; }
 };
@@ -32,53 +32,53 @@ class DiplomaStudent : public Student {
     string topic;
 public:
     DiplomaStudent() : Student(), topic("") {
-        cout << "DiplomaStudent() - конструктор за замовчуванням" << endl;
+        cout << "DiplomaStudent() - default constructor" << endl;
     }
     
     // Виклик конструктора базового класу
     DiplomaStudent(string s, string n, string p, int c, long long i, string t) 
         : Student(s, n, p, c, i), topic(t) {
-        cout << "DiplomaStudent(параметри) - створено дочiрнiй клас" << endl;
+        cout << "DiplomaStudent(parameters) - derived class created" << endl;
     }
     
-    ~DiplomaStudent() { cout << "~DiplomaStudent() - вилучення дочiрнього класу" << endl; }
+    ~DiplomaStudent() { cout << "~DiplomaStudent() - derived class destruction" << endl; }
 
     void print() const override {
         Student::print();
-        cout << "Тема диплома: " << topic << endl;
+        cout << "Diploma topic: " << topic << endl;
     }
     void setTopic(string new_topic) { topic = new_topic; }
 };
 
 void testTask1() {
-    cout << "\n--- Введення даних для Завдання 1.1 ---" << endl;
+    cout << "\n--- Data input for Task 1.1 ---" << endl;
     string s, n, p, t;
     int c;
     long long i;
 
-    cout << "Введiть прiзвище: "; cin >> s;
-    cout << "Введiть iм'я: "; cin >> n;
-    cout << "Введiть по-батьковi: "; cin >> p;
-    cout << "Введiть курс (число): "; cin >> c;
-    cout << "Введiть ID (число): "; cin >> i;
+    cout << "Enter surname: "; cin >> s;
+    cout << "Enter name: "; cin >> n;
+    cout << "Enter patronymic: "; cin >> p;
+    cout << "Enter course (number): "; cin >> c;
+    cout << "Enter ID (number): "; cin >> i;
     
     cin.ignore(); // Очищення буфера перед getline
-    cout << "Введiть тему диплома: "; getline(cin, t);
+    cout << "Enter diploma topic: "; getline(cin, t);
 
-    cout << "\n--- Створення об'єкта через конструктор з параметрами ---" << endl;
+    cout << "\n--- Creating object via parameterized constructor ---" << endl;
     DiplomaStudent ds(s, n, p, c, i, t);
     
-    cout << "\n--- Данi об'єкта ---" << endl;
+    cout << "\n--- Object data ---" << endl;
     ds.print();
 
-    cout << "\n--- Тест функцiй перепризначення ---" << endl;
-    cout << "Введiть новий ID: "; cin >> i;
+    cout << "\n--- Reassignment functions test ---" << endl;
+    cout << "Enter new ID: "; cin >> i;
     ds.setID(i);
     cin.ignore();
-    cout << "Введiть нову тему диплома: "; getline(cin, t);
+    cout << "Enter new diploma topic: "; getline(cin, t);
     ds.setTopic(t);
     
-    cout << "\nОновленi данi:" << endl;
+    cout << "\nUpdated data:" << endl;
     ds.print();
 }
 
@@ -101,12 +101,12 @@ public:
     
     OneRoomApartment(float r_area, float k_area, int fl) 
         : room(r_area), kitchenArea(k_area), floor(fl) {
-        cout << "OneRoomApartment(параметри) - базовий клас створено" << endl;
+        cout << "OneRoomApartment(parameters) - base class created" << endl;
     }
     
     virtual void print() const {
-        cout << "Поверх: " << floor << " | Кiмната: " << room.getArea() 
-             << " м2 | Кухня: " << kitchenArea << " м2" << endl;
+        cout << "Floor: " << floor << " | Room: " << room.getArea() 
+             << " m2 | Kitchen: " << kitchenArea << " m2" << endl;
     }
 };
 
@@ -117,31 +117,31 @@ public:
     
     CityApartment(float r, float k, int f, string c) 
         : OneRoomApartment(r, k, f), city(c) {
-        cout << "CityApartment(параметри) - дочiрнiй клас створено" << endl;
+        cout << "CityApartment(parameters) - derived class created" << endl;
     }
     
-    ~CityApartment() { cout << "~CityApartment() - вилучення" << endl; }
+    ~CityApartment() { cout << "~CityApartment() - destruction" << endl; }
 
     void print() const override {
-        cout << "Мiсто: " << city << " | ";
+        cout << "City: " << city << " | ";
         OneRoomApartment::print();
     }
 };
 
 void testTask2() {
-    cout << "\n--- Введення даних для Завдання 2.1 ---" << endl;
+    cout << "\n--- Data input for Task 2.1 ---" << endl;
     float r_area, k_area;
     int floor;
     string city;
 
-    cout << "Введiть площу кiмнати (м2): "; cin >> r_area;
-    cout << "Введiть площу кухнi (м2): "; cin >> k_area;
-    cout << "Введiть поверх: "; cin >> floor;
+    cout << "Enter room area (m2): "; cin >> r_area;
+    cout << "Enter kitchen area (m2): "; cin >> k_area;
+    cout << "Enter floor: "; cin >> floor;
     
     cin.ignore();
-    cout << "Введiть назву мiста: "; getline(cin, city);
+    cout << "Enter city name: "; getline(cin, city);
 
-    cout << "\n--- Створення об'єкта ---" << endl;
+    cout << "\n--- Object creation ---" << endl;
     CityApartment flat(r_area, k_area, floor, city);
     flat.print();
 }
@@ -152,8 +152,8 @@ protected:
     string* name;
 public:
     SportGame(string n = "") : name(new string(n)) {}
-    SportGame(const SportGame& other) : name(new string(*other.name)) { cout << "SportGame: Конструктор копiювання" << endl; }
-    SportGame(SportGame&& other) noexcept : name(other.name) { other.name = nullptr; cout << "SportGame: Конструктор перенесення" << endl; }
+    SportGame(const SportGame& other) : name(new string(*other.name)) { cout << "SportGame: Copy constructor" << endl; }
+    SportGame(SportGame&& other) noexcept : name(other.name) { other.name = nullptr; cout << "SportGame: Move constructor" << endl; }
     
     virtual ~SportGame() { delete name; }
 
@@ -170,7 +170,7 @@ public:
 
     // Перевизначення вводу (для базового класу)
     friend istream& operator>>(istream& is, SportGame& g) {
-        cout << "Введiть назву гри (базовий клас): ";
+        cout << "Enter game name (base class): ";
         string temp;
         is >> temp;
         if (g.name) *g.name = temp;
@@ -185,26 +185,26 @@ public:
     Football() : SportGame(), stadium(new string("")) {}
     
     Football(const Football& other) : SportGame(other), stadium(new string(*other.stadium)) {
-        cout << "Football: Конструктор копiювання" << endl;
+        cout << "Football: Copy constructor" << endl;
     }
     
     Football(Football&& other) noexcept : SportGame(std::move(other)), stadium(other.stadium) {
         other.stadium = nullptr;
-        cout << "Football: Конструктор перенесення" << endl;
+        cout << "Football: Move constructor" << endl;
     }
 
     ~Football() { delete stadium; }
 
     // Перевизначення виводу з викликом базового
     friend ostream& operator<<(ostream& os, const Football& f) {
-        os << (const SportGame&)f << " | Стадiон: " << *f.stadium;
+        os << (const SportGame&)f << " | Stadium: " << *f.stadium;
         return os;
     }
 
     // Перевизначення вводу з викликом базового
     friend istream& operator>>(istream& is, Football& f) {
         is >> (SportGame&)f; // Викликаємо введення базового класу
-        cout << "Введiть назву стадiону (дочiрнiй клас): ";
+        cout << "Enter stadium name (derived class): ";
         string temp;
         is >> temp;
         if (f.stadium) *f.stadium = temp;
@@ -214,41 +214,40 @@ public:
 };
 
 void testTask3() {
-    cout << "\n--- Тест Завдання 3.1 (Перевантаження >> та <<) ---" << endl;
+    cout << "\n--- Test Task 3.1 (Overloading >> and <<) ---" << endl;
     Football f1;
     cin >> f1; // Працює перевантажений оператор введення!
 
-    cout << "\nОригiнальний об'єкт: " << f1 << endl;
+    cout << "\nOriginal object: " << f1 << endl;
     
-    cout << "\nТест копiювання:" << endl;
+    cout << "\nCopy test:" << endl;
     Football f2 = f1; 
-    cout << "Копiя f2: " << f2 << endl;
+    cout << "Copy f2: " << f2 << endl;
 
-    cout << "\nТест перенесення:" << endl;
+    cout << "\nMove test:" << endl;
     Football f3 = std::move(f1); 
-    cout << "Об'єкт f3 пiсля перенесення: " << f3 << endl;
+    cout << "Object f3 after move: " << f3 << endl;
 }
 
 
 int main() {
-    setlocale(LC_CTYPE, "ukr");
     char choice;
 
     do {
-        cout << "\n=========== ГОЛОВНЕ МЕНЮ ===========" << endl;
-        cout << "1. Завдання 1.1 (Спадкування)" << endl;
-        cout << "2. Завдання 2.1 (Композицiя)" << endl;
-        cout << "3. Завдання 3.1 (Ввiд/Вивiд у потiк)" << endl;
-        cout << "q. Вихiд" << endl;
-        cout << "Ваш вибiр: ";
+        cout << "\n   MAIN MENU" << endl;
+        cout << "1. Task 1.1 " << endl;
+        cout << "2. Task 2.1 " << endl;
+        cout << "3. Task 3.1 " << endl;
+        cout << "q. Exit" << endl;
+        cout << "Your choice: ";
         cin >> choice;
 
         switch (choice) {
             case '1': testTask1(); break;
             case '2': testTask2(); break;
             case '3': testTask3(); break;
-            case 'q': cout << "Програму завершено." << endl; break;
-            default: cout << "Помилка: оберiть пункт вiд 1 до 3 або q." << endl;
+            case 'q': cout << "Program terminated." << endl; break;
+            default: cout << "Error: choose an option from 1 to 3 or q." << endl;
         }
     } while (choice != 'q');
 
